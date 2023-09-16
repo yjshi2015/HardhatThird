4e3cc9416e210250f173e76905d49f4c1bd225a9
yjshi <495054021@qq.com>
2023-09-16 22:18:33 +0800

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
-const {keyConfig} = require('./scripts/keyConfig.json');
+const keyConfig = require('./scripts/keyConfig.json');
 // Go to https://alchemy.com, sign up, create a new App in
 // its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "2vsw2JgOi6Hq-";
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -10,8 +9,7 @@ const ALCHEMY_API_KEY = "2vsw2JgOi6Hq-";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-// todo syj delete it after used
-const SEPOLIA_PRIVATE_KEY = "";
+
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -25,8 +23,8 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: keyConfig.RPCProvider["sepolia.testnet"],
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      url: keyConfig.RPCProvider.sepolica_testnet,
+      accounts: keyConfig.account
     }
   }
 };
